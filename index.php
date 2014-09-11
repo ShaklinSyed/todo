977f10b6bb4560bfd0c0028b6b25c3f6507f4251
ShaklinSyed <shaklin_s@yahoo.com>
2014-09-11 16:42:41 +0530

    view from database

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+
+include_once 'lib.php';
+
 echo <<<_END
 <!DOCTYPE html>
 
@@ -26,32 +29,51 @@ _END;
 		}	
 
 if(isset($_POST['inp0']) || isset($_POST['inp1']) || isset($_POST['inp2'])){
-	
 	$val1 = sanitizeString($_POST['inp0']);
 	$val2 = sanitizeString($_POST['inp1']);
 	$val3 = sanitizeString($_POST['inp2']);
-
-
-	// if(!empty($val1)){
-	// 	if(!empty($val2)){
-
-	// 	}
-	// 	else if(!empty)
-	// }
-	// else if(!empty($val2)){
-	// 	$str 
-	// }
-	// else{
-
-	// }
-
-}
-
+		//Stores the data
+		if(!empty($val1)){
+			$query = "INSERT INTO todo VALUES ('','$val1','no')";
+			queryMySql($query);
+		}		
+		if(!empty($val2)){
+			$query = "INSERT INTO todo VALUES ('','$val2','no')";
+			queryMySql($query);
+		}		
+		if(!empty($val3)){
+			$query = "INSERT INTO todo VALUES ('','$val3','no')";
+			queryMySql($query);
+		}
+	}
 
 echo "	
 		<input type='submit' value='Add' />	
 		</form>
+
+		<div id='incomplete'> <form method='POST' action='index.php'>";
+			
+			$query = "SELECT * FROM todo WHERE status='no'";
+			$result = queryMySql($query);
+			$num = mysql_num_rows($result);
+
+			for($i=0;$i<$num;$i++){
+				$row = mysql_fetch_row($result);
+
+				echo "<input type='checkbox' name='list' />
+				<label>$row[1]</label><br /> ";
+			}
+echo '
+		<input type="submit" value="Completed" />
+
+
+		</form>;
+		</div>
+
 	</body>
-</html>";
+</html>';
+
+
+	if(isset($_POST[]))
 ?>
 
